040105b5fd3e4668187c001c30fe3b7289c69642
Govetachun <xuanbach1809@gmail.com>
2022-12-03 21:03:46 +0700

    asdad

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import graphviz
+import time
+from streamlit_chat import message
+import requests
+st.set_page_config(
+page_title = "Real-Time Data Dashboard",
+page_icon = "Active",
+layout = "wide",
+initial_sidebar_state='expanded',
+)
+
+
 if os.path.exists('./dataset.csv'): 
     df = pd.read_csv('dataset.csv', index_col=None)
 if os.path.exists('./workshift.csv'): 
@@ -20,15 +31,16 @@ if os.path.exists('./routes2.csv'):
     routes2 = pd.read_csv('routes2.csv', index_col=None)
 if os.path.exists('./routes3.csv'): 
     routes3 = pd.read_csv('routes3.csv', index_col=None)
-st.set_page_config(
-page_title = "Real-Time Data Dashboard",
-page_icon = "Active",
-layout = "wide",
-)
+if os.path.exists('./vehicle.csv'): 
+    vehicle = pd.read_csv('vehicle.csv', index_col=None)
+if os.path.exists('./vehicle_assigning.csv'): 
+    vehicle_assigning = pd.read_csv('vehicle_assigning.csv', index_col=None)
+if os.path.exists('./MCP_assigning.csv'): 
+    MCP_assigning = pd.read_csv('MCP_assigning.csv', index_col=None)
 with st.sidebar: 
     st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
     st.title("UWC for task assignment")
-    choice = st.radio("Navigation", ["Upload","Dashboard","Calendar", "MCP","Tasks Assignment","Routes Planning"])
+    choice = st.radio("Navigation", ["Upload","Dashboard","Calendar", "Vehicles","MCP","Tasks Assignment","Routes Planning"])
     st.info("UWC")
 
 if choice == "Upload":
@@ -41,47 +53,64 @@ if choice == "Upload":
 
 elif choice == "Dashboard":
     st.title("Dashboard for the work day")
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.tabs(['Morning','Afternoon','Evening'])
     # Pie chart, where the slices will be ordered and plotted counter-clockwise:
     with col1:
-        st.info("Current status of garbage trucks in Morning")
-        colors = ['gold', 'mediumturquoise', 'darkorange']
-        value = df[:3].values[0]
-        fig = go.Figure(data=[go.Pie(labels=['In use','In use','Available','In maintenance'], values=value)])
-        fig.update_traces(hoverinfo='label+percent', textinfo='label+value', textfont_size=15,
-                    marker=dict(colors=colors, line=dict(color='#000000', width=3)))
+        st.markdown("### Current status of garbage trucks in Morning")
+        column1,column2,column3=st.columns(3)
+        with column1:
+            st.write('The vehicles status available')
+            colors = ['gold', 'mediumturquoise', 'darkorange']
+            value = df[:3].values[0]
+            fig = go.Figure(data=[go.Pie(labels=['In use','In use','Available','In maintenance'], values=value,title="Vehicles")])
+            fig.update_traces(hoverinfo='label+percent', textinfo='label+value', textfont_size=15,
+                        marker=dict(colors=colors, line=dict(color='#000000', width=3)))
 
-        st.write(fig)
-        df1 = pd.DataFrame()
-        fig2 = ff.create_table(df1)
-        teams=['Morning']
-        y1 = [df['Collectors'].values[0]]
-        y2 = [df['Janitors'].values[0]]
-        trace1 = go.Bar(x=teams, y=y1, xaxis='x2', yaxis='y2',
-                marker=dict(color='#0099ff'),
-                name='Collectors')
-        trace2 = go.Bar(x=teams, y=y2, xaxis='x2', yaxis='y2',
-                marker=dict(color='#404040'),
-                name='Janitors')
-        fig2.add_traces([trace1, trace2])
-        fig2['layout']['xaxis2'] = {}
-        fig2['layout']['yaxis2'] = {}
-        fig2.layout.yaxis2.update({'anchor': 'x2'})
-        fig2.layout.xaxis2.update({'anchor': 'y2'})
-        fig2.layout.yaxis2.update({'title': 'people'})
+            st.write(fig)
+        with column2:
+            st.write("MCPs list status in Morning")
+            fig3 = px.bar(cap,x="MCP",y="count",color="status")
+            st.write(fig3)
+        with column3:
+            df1 = pd.DataFrame()
+            fig2 = ff.create_table(df1)
+            st.write("Workshift today")
+            teams=['Morning']
+            y1 = [df['Collectors'].values[0]]
+            y2 = [df['Janitors'].values[0]]
+            trace1 = go.Bar(x=teams, y=y1, xaxis='x2', yaxis='y2',
+                    marker=dict(color='#0099ff'),
+                    name='Collectors')
+            trace2 = go.Bar(x=teams, y=y2, xaxis='x2', yaxis='y2',
+                    marker=dict(color='#404040'),
+                    name='Janitors')
+            fig2.add_traces([trace1, trace2])
+            fig2['layout']['xaxis2'] = {}
+            fig2['layout']['yaxis2'] = {}
+            fig2.layout.yaxis2.update({'anchor': 'x2'})
+            fig2.layout.xaxis2.update({'anchor': 'y2'})
+            fig2.layout.yaxis2.update({'title': 'people'})
 
-        st.write(fig2)
+            st.write(fig2)
 
-        st.info("Assignment")
-        st.dataframe(wsh)
 
-        st.info("MCPs list")
-        fig3 = px.bar(cap,x="MCP",y="count",color="status", title="MCPs list status in Morning")
-        st.write(fig3)
+        column4,column5= st.columns((3,7))
+        with column4:
+            st.markdown("### Vehicle status")
+
+            def highlight_survived(s):
+                return ['background-color: green']*len(s) if s.Available else ['background-color: red']*len(s)
+            st.dataframe(vehicle.style.apply(highlight_survived, axis=1))
+        with column5:
+            st.markdown("### Assignment")
+            fig5 = ff.create_table(wsh)
+            st.write(fig5)
+            
 
 
     with col2:
         st.info("Current status of garbage trucks in Afternoon")
+        st.title("Vehicle status")
         colors = ['red', 'green', 'blue']
         value = df[:3].values[1]
         fig = go.Figure(data=[go.Pie(labels=['In use','In use','Available','In maintenance'], values=value)])
@@ -184,3 +213,69 @@ elif choice == "Dashboard":
             graph1.edge(routes1['A'].values[i],routes1['B'].values[i])
 
         st.graphviz_chart(graph1)
+elif choice == "Vehicles":
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    st.markdown('### Vehicle')
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Vehicle number: 1234", "0km/h", "Available")
+    col2.metric("Vehicle number: 2345", "20km/h", "-In use")
+    col3.metric("Vehicle number: 3456", "40km/h", "-In use%")
+    col4, col5, col6 = st.columns(3)
+    col4.metric("Vehicle number: 1234", "70km/h", "-In use")
+    col5.metric("Vehicle number: 2345", "0km/h", "Available")
+    col6.metric("Vehicle number: 3456", "8km/h", "-In use")
+    col7, col8, col9 = st.columns(3)
+    col7.metric("Vehicle number: 1234", "30km/h", "-In use")
+    col8.metric("Vehicle number: 2345", "22km/h", "-In use")
+    col9.metric("Vehicle number: 3456", "0km/h", "Available")
+    col10, col11, col12 = st.columns(3)
+    col10.metric("Vehicle number: 1234", "30km/h", "-In use")
+    col11.metric("Vehicle number: 2345", "22km/h", "Available")
+    col12.metric("Vehicle number: 3456", "0km/h", "Available")
+elif choice == "MCP":
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    st.markdown('### Vehicle')
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.markdown('### #MCP1 ')
+        st.markdown('#####  268 Ly Thuong Kiet')
+        age = st.slider('Capacity', 0, 100, 25)
+        st.write( age, '% of the capacity')
+    with col2:
+        st.markdown('### #MCP2 ')
+        st.markdown('#####  114 To Hien Thanh')
+        age2 = st.slider('Capacity', 0, 100, 26)
+        st.write( age2, '% of the capacity')
+    with col3:
+        st.markdown('### #MCP1 ')
+        st.markdown('#####  153 Nguyen Chi Thanh')
+        age3 = st.slider('Capacity', 0, 100, 50)
+        st.write( age3, '% of the capacity')
+    col4,col5,col6 = st.columns(3)
+    with col4:
+        st.markdown('### #MCP4 ')
+        st.markdown('#####  100 Tran Hung Dao')
+        age4 = st.slider('Capacity', 0, 100, 70)
+        st.write( age4, '% of the capacity')
+    with col5:
+        st.markdown('### #MCP5 ')
+        st.markdown('#####  45 Nguyen Thai Hoc')
+        age5 = st.slider('Capacity', 0, 100, 95)
+        st.write( age5, '% of the capacity')
+    with col6:
+        st.markdown('### #MCP6 ')
+        st.markdown('#####  34 Cong Hoa')
+        age6 = st.slider('Capacity', 0, 100, 12)
+        st.write( age6, '% of the capacity')
+elif choice == "Tasks Assignment":
+
+    
+    col1 ,col2,col3 = st.columns(3)
+    with col1:
+        st.markdown('### Vehicle assigning')
+        st.dataframe(vehicle_assigning)
+    with col2:
+        st.markdown('### MCPs assigning')
+        st.dataframe(MCP_assigning)
--- create_csv.py
+++ create_csv.py
@@ -1,105 +1,187 @@
 import csv
+def today():
+    header = ['Time', 'In use', 'Available', 'In maintenance','Collectors','Janitors']
+    data = [
+        ['Morning', 3, 4, 5,3,12],
+        ['Afternoon', 4, 2, 6, 4, 14],
+        ['Evening', 3, 5, 4, 3, 10]
+    ]
 
-header = ['Time', 'In use', 'Available', 'In maintenance','Collectors','Janitors']
-data = [
-    ['Morning', 3, 4, 5,3,12],
-    ['Afternoon', 4, 2, 6, 4, 14],
-    ['Evening', 3, 5, 4, 3, 10]
-]
+    with open('today.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-with open('today.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+        # write the header
+        writer.writerow(header)
 
-    # write the header
-    writer.writerow(header)
+        # write multiple rows
+        writer.writerows(data)
+today()
 
-    # write multiple rows
-    writer.writerows(data)
+def workshift():
+    header = ['Vehicle', 'Collector', 'MCP', 'Jantitor 1','Janitor 2','Janitor 3']
+    data = [
+        ['#1234', 'Nguyen Van A','MCP 1','Tran Van A','Tran Van B','Tran Thi X'],
+        ['#0001', 'Nguyen Van B','MCP 2','Tran Van C','Tran Van D','Tran Thi Y'],
+        ['#0004', 'Nguyen Van C','MCP 3','Tran Van E','Tran Van F','Tran Thi Z'],
+        ['#0005', 'Nguyen Van G','MCP 4','Tran Van E','Tran Van F','Tran Thi Z'],
+        ['#0006', 'Nguyen Van H','MCP 5','Tran Van E','Tran Van F','Tran Thi Z'],
+        ['#0007', 'Nguyen Van I','MCP 6','Tran Van E','Tran Van F','Tran Thi Z']
+    ]
 
-header = ['Vehicle', 'Collector', 'MCP', 'Jantitor 1','Janitor 2']
-data = [
-    ['#1234', 'Nguyen Van A','MCP 1','Tran Van A','Tran Van B'],
-    ['#0001', 'Nguyen Van B','MCP 2','Tran Van C','Tran Van D'],
-    ['#0004', 'Nguyen Van C','MCP 3','Tran Van E','Tran Van F']
-]
+    with open('workshift.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-with open('workshift.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+        # write the header
+        writer.writerow(header)
 
-    # write the header
-    writer.writerow(header)
+        # write multiple rows
+        writer.writerows(data)
+workshift()
+def capacity():
+    header = ['MCP', 'status','count']
+    data = [
+        ['MCP 1', 'Full',4],
+        ['MCP 1', 'Available',16],
+        ['MCP 2', 'Full',5],
+        ['MCP 2', 'Available',15],
+        ['MCP 3', 'Full',19],
+        ['MCP 3', 'Available',6],
+        ['MCP 4', 'Full',10],
+        ['MCP 4', 'Available',10],
+        ['MCP 5', 'Full',12],
+        ['MCP 5', 'Available',18],
+    ]
 
-    # write multiple rows
-    writer.writerows(data)
+    with open('capacity.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-header = ['MCP', 'status','count']
-data = [
-    ['MCP 1', 'Full',4],
-    ['MCP 1', 'Available',16],
-    ['MCP 2', 'Full',5],
-    ['MCP 2', 'Available',15],
-    ['MCP 3', 'Full',19],
-    ['MCP 3', 'Available',1],
-]
+        # write the header
+        writer.writerow(header)
 
-with open('capacity.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+        # write multiple rows
+        writer.writerows(data)
+capacity()
+def routes1():
+    header = [ 'A','B']
+    data = [
+        [ 'Quan 1','Quan 2'],
+        [ 'Quan 2','Quan 3'],
+        [ 'Quan 3', 'Quan 5'],
+        [ 'Quan 5','Quan 12'],
+        [ 'Quan 12','Quan 4'],
+        [ 'Quan 4','Quan 1']
+    ]
 
-    # write the header
-    writer.writerow(header)
+    with open('routes1.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-    # write multiple rows
-    writer.writerows(data)
+        # write the header
+        writer.writerow(header)
 
-header = [ 'A','B']
-data = [
-    [ 'Quan 1','Quan 2'],
-    [ 'Quan 2','Quan 3'],
-    [ 'Quan 3', 'Quan 5'],
-    [ 'Quan 5','Quan 12'],
-    [ 'Quan 12','Quan 4'],
-    [ 'Quan 4','Quan 1']
-]
+        # write multiple rows
+        writer.writerows(data)
+routes1()
+def routes2():
+    header = [ 'A','B']
+    data = [
+        [ 'Quan 1','Quan 2'],
+        [ 'Quan 2','Quan 3'],
+        [ 'Quan 3', 'Quan 5'],
+        [ 'Quan 5','Quan 12'],
+        [ 'Quan 12','Quan 1'],
+    ]
 
-with open('routes1.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+    with open('routes2.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-    # write the header
-    writer.writerow(header)
+        # write the header
+        writer.writerow(header)
 
-    # write multiple rows
-    writer.writerows(data)
+        # write multiple rows
+        writer.writerows(data)
+routes2()
 
-header = [ 'A','B']
-data = [
-    [ 'Quan 1','Quan 2'],
-    [ 'Quan 2','Quan 3'],
-    [ 'Quan 3', 'Quan 5'],
-    [ 'Quan 5','Quan 12'],
-    [ 'Quan 12','Quan 1'],
-]
+def routes3():
+    header = [ 'A','B']
+    data = [
+        [ 'Quan 1','Quan 2'],
+        [ 'Quan 2','Quan 3'],
+        [ 'Quan 3', 'Quan 5'],
+    ]
 
-with open('routes2.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+    with open('routes3.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-    # write the header
-    writer.writerow(header)
+        # write the header
+        writer.writerow(header)
 
-    # write multiple rows
-    writer.writerows(data)
+        # write multiple rows
+        writer.writerows(data)
+routes3()
+def vehicle():
+    header = [ 'ID','Available','Current location']
+    data = [
+        [ '#1234',1,'MCP 1'],
+        [ '#0001',1,'MCP 3'],
+        [ '#0004',0, 'MCP 5'],
+        [ '#2345', 1,'MCP 1'],
+        [ '#3456',0, 'Station 3'],
+        [ '#4567',0, 'Station 11'],
+        [ '#5678',1, 'Station 5'],
+        [ '#9787',0, 'MCP 2'],
+    ]
 
-header = [ 'A','B']
-data = [
-    [ 'Quan 1','Quan 2'],
-    [ 'Quan 2','Quan 3'],
-    [ 'Quan 3', 'Quan 5'],
-]
+    with open('vehicle.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
 
-with open('routes3.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+        # write the header
+        writer.writerow(header)
 
-    # write the header
-    writer.writerow(header)
+        # write multiple rows
+        writer.writerows(data)
+vehicle()
 
-    # write multiple rows
-    writer.writerows(data)
+def vehicle_assigning():
+    header = [ 'Vehicle ID','Collector']
+    data = [
+        [ '#1234','Nguyen Van A'],
+        [ '#0001','Nguyen Van B'],
+        [ '#0004', 'Nguyen Van C'],
+        [ '#2345', 'Nguyen Van D'],
+        [ '#3456', 'Nguyen Van E'],
+        [ '#4567', 'Nguyen Van F'],
+        [ '#5678', 'Nguyen Van G'],
+        [ '#9787', 'Nguyen Van H'],
+    ]
+
+    with open('vehicle_assigning.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
+
+        # write multiple rows
+        writer.writerows(data)
+vehicle_assigning()
+def MCP_assigning():
+    header = [ 'MCP ID','Janitor 1', 'Janitor 2']
+    data = [
+        [ 'MCP 1','Nguyen Van A','Tran Van A'],
+        [ 'MCP 2','Nguyen Van B','Tran Van B'],
+        [ 'MCP 3', 'Nguyen Van C','Tran Van C'],
+        [ 'MCP 4', 'Nguyen Van D','Tran Van D'],
+        [ 'MCP 5', 'Nguyen Van E','Tran Van E'],
+        [ 'MCP 6', 'Nguyen Van F','Tran Van F'],
+        [ 'MCP 7', 'Nguyen Van F','Tran Van F'],
+        [ 'MCP 8', 'Nguyen Van F','Tran Van F'],
+    ]
+
+    with open('MCP_assigning.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
+
+        # write multiple rows
+        writer.writerows(data)
+MCP_assigning()
